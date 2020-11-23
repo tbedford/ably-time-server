eada1c9bddeb5788a4858972379d67dd29eb9078
Tony Bedford <tony.bedford@live.co.uk>
2020-11-23 11:23:33 +0000

    Add IoT client

---
+++ client/iot.js
@@ -0,0 +1,53 @@
+// A simple IoT client that uses JWTs for authentication
+
+const https = require("https");
+const { exit } = require("process");
+
+var jwt = null;
+
+const options = {
+  hostname: "flawless-buttery-legal.glitch.me",
+  port: 443,
+  path: "/jwt",
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    "User-Agent": "IoT client v0.1",
+  },
+};
+
+const req = https.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`);
+
+  res.on("data", (d) => {
+    jwt = d.toString();
+    console.log(jwt);
+
+    const ably = new require("ably").Realtime(jwt);
+
+    ably.connection.on("connecting", function () {
+      console.log("Connecting to Ably...");
+    });
+    ably.connection.on("connected", function () {
+      console.log("Connected");
+    });
+    ably.connection.on("disconnected", function () {
+      console.log("Disconnected from Ably...");
+    });
+    ably.connection.on("suspended", function () {
+      console.log("Disconnected from Ably for a while (suspended)...");
+    });
+
+    const channel = ably.channels.get("time-server");
+
+    channel.subscribe("time", function (message) {
+      console.log("Time on server is: " + message.data);
+    });
+  });
+});
+
+req.on("error", (error) => {
+  console.error(error);
+});
+
+req.end();
