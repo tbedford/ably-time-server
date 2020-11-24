b50de489b46fe02420f7b27db669f2fbb59b96a9
Tony Bedford <tony.bedford@live.co.uk>
2020-11-24 16:21:12 +0000

    Also test locally

--- client/iot-auth-url.js
+++ client/iot-auth-url.js
@@ -1,5 +1,6 @@
 const Ably = require("ably");
-const auth_url = "https://flawless-buttery-legal.glitch.me/auth";
+//const auth_url = "https://flawless-buttery-legal.glitch.me/auth"; // doesn;t work
+const auth_url = "http://localhost:8000/jwt"; // does work, but both auth servers use same code
 
 var ably = new Ably.Realtime({authUrl: auth_url});
 
