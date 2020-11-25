9bc3423b4189c82642043450171ea04be1135c70
Tony Bedford <tony.bedford@live.co.uk>
2020-11-25 09:36:21 +0000

    Working auth callback

--- client/iot-callback.js
+++ client/iot-callback.js
@@ -3,42 +3,41 @@ const https = require("https");
 const options = {
   hostname: "flawless-buttery-legal.glitch.me",
   port: 443,
-  path: "/jwt",
+  path: "/jwt",  // You can also change this to /auth and it will then work with TokenRequest rather than Ably JWT 
   method: "GET",
   headers: {
     // will fail if you don't set this
     "User-Agent": "IoT client v0.1",
   },
 };
-function doRequest() {
-  const req = https.request(options, (res) => {
-    console.log(`statusCode: ${res.statusCode}`);
-    if (res.statusCode == 200) {
-      res.on("data", (d) => {
-        jwt = d.toString();
-        jwt = JSON.parse(jwt);
-        console.log(jwt);
-      });
-    } else {
-      console.error("Response code was not 200.");
-    }
-  });
-
-  // error making request
-  req.on("error", (error) => {
-    console.log("Something went bang...");
-    console.error(error);
-  });
-
-  req.end();
-}
-
-doRequest();
 
 // function(tokenParams, callback(err, tokenOrTokenRequest))
 
-/*
-const ably = new require("ably").Realtime({authCallback: getToken});
+const ably = new require("ably").Realtime({
+  authCallback: function (tokenParams, callback) {
+    const req = https.request(options, (res) => {
+      console.log(`statusCode: ${res.statusCode}`);
+      if (res.statusCode == 200) {
+        res.on("data", (d) => {
+          token = d.toString();
+          token = JSON.parse(token);
+          callback(null, token);
+        });
+      } else {
+        console.error("Response code was not 200.");
+        callback(res.statusCode, null)
+      }
+    });
+
+    // error making request
+    req.on("error", (error) => {
+      console.log("Something went bang...");
+      console.error(error);
+    });
+
+    req.end();
+  },
+});
 
 ably.connection.on("connecting", function () {
   console.log("Connecting to Ably...");
@@ -47,4 +46,9 @@ ably.connection.on("connecting", function () {
 ably.connection.on("connected", function () {
   console.log("Connected");
 });
-*/
+
+const channel = ably.channels.get("time-server");
+
+channel.subscribe("time", function (message) {
+  console.log("Time on server is: " + message.data);
+});
